46c4943471efdceca96038dc7894ec355c90985c
amitkumarjaiswal-novopay <amitkumarjaiswal-novopay@novopay.in>
2020-12-16 18:17:48 +0530

    Robolectric Setup And Minor Changes

--- app/src/main/java/com/carlsberg_stack/robolectricsampletest/MainActivity.kt
+++ app/src/main/java/com/carlsberg_stack/robolectricsampletest/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.carlsberg_stack.robolectricsampletest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var login = findViewById<Button>(R.id.login);
+        var emailAddress = findViewById<EditText>(R.id.email_address);
+        login.setOnClickListener {
+            if(Validator.validateEmail(emailAddress.text.toString())){
+                val intent = Intent(this, HomeActivity::class.java);
+                startActivity(intent)
+            } else{
+//                emailAddress.error = getString(R.string.err_invalid_email_address)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/carlsberg_stack/robolectricsampletest/Validator.java
@@ -0,0 +1,7 @@
+package com.carlsberg_stack.robolectricsampletest;
+
+public class Validator {
+    public static boolean validateEmail(String emailAddress) {
+        return false;
+    }
+}
